320ad4ee271f66647f1a1a6c39671fa8805e3861
Micah Schuster <micahdschuster@gmail.com>
2018-04-20 22:59:57 -0400

    TextGrid and Paddle dynamically change size with screen

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Manager : MonoBehaviour {
+     
+    //for text grid
+    GameObject textGrid;
+    GameObject Paddle;
+    Text[] theText;
+    int prevH;
+    int prevW;
+
+	// Use this for initialization
+	void Start () {
+        textGrid = GameObject.Find("TextGrid");
+        Paddle = GameObject.Find("Paddle");
+        textGrid.GetComponent<GridLayoutGroup>().cellSize = new Vector2(Camera.main.pixelWidth / 6f, Camera.main.pixelHeight / 12f);
+        theText = textGrid.GetComponentsInChildren<Text>();
+        theText[0].text = "Zero";
+        theText[1].text = "One";
+        theText[2].text = "Two";
+        theText[3].text = "Three";
+        theText[4].text = "Four";
+        theText[5].text = "Five";
+
+
+        float screenWidth=Camera.main.pixelWidth / 6f;
+        Vector3 paddleScale = Paddle.transform.localScale;
+        paddleScale.x = screenWidth * unitsPerPixel();
+        Paddle.transform.localScale = paddleScale;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (prevH != Camera.main.pixelHeight || prevW != Camera.main.pixelWidth) {
+            //change the text grid to still be proportional to the screensize
+            textGrid.GetComponent<GridLayoutGroup>().cellSize = new Vector2(Camera.main.pixelWidth / 6f, Camera.main.pixelHeight / 12f);
+
+            //change paddle size
+            float screenWidth = Camera.main.pixelWidth / 6f;
+            Vector3 paddleScale = Paddle.transform.localScale;
+            paddleScale.x = screenWidth * unitsPerPixel();
+            Paddle.transform.localScale = paddleScale;
+        }
+
+
+
+    }
+
+
+    public static float unitsPerPixel() {
+        var p1 = Camera.main.ScreenToWorldPoint(Vector3.zero);
+        var p2 = Camera.main.ScreenToWorldPoint(Vector3.right);
+        return Vector3.Distance(p1, p2);
+    }
+
+    public static float pixelsPerUnit() {
+        return 1 / unitsPerPixel();
+    }
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    private bool isEnabled = true;
+    public Vector2 vel;
+    private Vector2 dir;
+    // Use this for initialization
+    void Start() {
+        dir = new Vector2(0f, -1f);
+        vel = new Vector2(0f, -5f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!isEnabled) {
+            return;
+        }
+
+        move();
+
+    }
+
+    private void move() {
+        Vector2 pos = this.transform.position;
+        Vector2 newPos = new Vector2(0f, 0f);
+        checkBoundry(pos);
+        //x
+        newPos.x = pos.x + vel.x * Time.deltaTime;
+        //y
+        newPos.y = pos.y + vel.y * Time.deltaTime;
+
+        this.transform.position = newPos;
+    }
+
+    private void checkBoundry(Vector2 myPos) {
+        Vector2 pixScale = Camera.main.WorldToScreenPoint(new Vector2(0,0.5f));
+        //TODO: Ball moves halfway out of screen, fix to have ball appear to hit
+        //the boundry at edge
+
+        //Debug.Log(pixScale+" "+myPos);
+        Vector2 pos = Camera.main.WorldToScreenPoint(myPos);
+        if (pos.y < 0) {
+            vel.y = -vel.y;
+        } else if (pos.y > Camera.main.pixelHeight) {
+            vel.y = -vel.y;
+        } else if (pos.x < 0) {
+            vel.x = -vel.x;
+        } else if (pos.x > Camera.main.pixelWidth) {
+            vel.x = -vel.x;
+        }
+    }
+
+            void OnTriggerEnter(Collider other) {
+        Debug.Log("Hit");
+        vel.y = -vel.y;
+        vel.x = -vel.x;
+    }
+}
