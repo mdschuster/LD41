8274834c32812e2c895dd895fd4b0c2d3b155bfe
Micah Schuster <micahdschuster@gmail.com>
2018-04-22 09:31:16 -0400

    Added dynamic boundies for window resize

---
+++ Assets/Scripts/BoundryManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundryManager : MonoBehaviour {
+
+    public GameObject Right;
+    public GameObject Left;
+    private int prevWidth;
+	// Use this for initialization
+	void Start () {
+        int prevWidth = Camera.main.pixelWidth;
+        adjustSize();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (prevWidth != Camera.main.pixelWidth) {
+            prevWidth = Camera.main.pixelWidth;
+            adjustSize();
+        }
+		
+	}
+
+    void adjustSize() {
+        Vector3 posR = Right.transform.position;
+        int pixelWidth = Camera.main.pixelWidth;
+        posR.x = pixelWidth * Manager.unitsPerPixel() / 2f + Right.transform.localScale.x / 4f;
+        Right.transform.position = posR;
+
+        Vector3 posL = Left.transform.position;
+        posL.x = -pixelWidth * Manager.unitsPerPixel() / 2f - Left.transform.localScale.x / 4f;
+        Left.transform.position = posL;
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Debug.Log(Camera.main.pixelHeight + " " + Camera.main.pixelWidth);
+        //Screen.SetResolution(645, 652, false);
+    }
+	
+    public void onClickPlay() {
+        SceneManager.UnloadSceneAsync("MainMenu");
+        SceneManager.LoadScene("_SCENE_");
+
+    }
+
+    public void onClickExit() {
+        Application.Quit();
+    }
+}
